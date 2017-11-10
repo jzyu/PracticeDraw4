c8657e115e8a64871258c153285b6bab28ef247d
jzyu@iMac <971118309@qq.com>
2017-11-10 19:05:21 +0800

    homework :)

--- app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice01ClipRectView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice01ClipRectView.java
@@ -38,6 +38,13 @@ public class Practice01ClipRectView extends View {
         int left = (getWidth() - bitmap.getWidth()) / 2;
         int top = (getHeight() - bitmap.getHeight()) / 2;
 
+        canvas.save();
+        canvas.clipRect(
+                left + 35,
+                top + 50,
+                left + bitmap.getWidth() - 20,
+                top + bitmap.getHeight() - 100);
         canvas.drawBitmap(bitmap, left, top, paint);
+        canvas.restore();
     }
 }
